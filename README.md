cb5c35ae2ddd2233ba4614419a0cc8e823b76d6d
Isaac Max <110857429+JIsaacMax@users.noreply.github.com>
2025-12-16 11:56:01 +0000

    Padronização do editor e adição de lint

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Uma forma de estudo usando um projeto real de fora a fora.
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,7 @@
 export default function Home() {
-    return <h1>O titulo do meu Home é isso aqui mesmo</h1>;
+  return <h1>O titulo do meu Home é isso aqui mesmo</h1>;
+}
+
+function teste() {
+  return "coisas";
 }
